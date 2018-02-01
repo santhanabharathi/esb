25016aa48f7cc9d1834325f6c1813fad6c236140
kongu bharathi <31886062+santhanabharathi@users.noreply.github.com>
2018-02-01 10:42:22 +0000

    Create validate.c

---
+++ validate.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("\n enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("\n string numeric");
+}
+else
+{
+printf("\n not a numeric);
+}
+return 0;
+}
