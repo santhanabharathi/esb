79c353846903171adb3ce855eea0e255ed8c31b3
kongu bharathi <31886062+santhanabharathi@users.noreply.github.com>
2018-02-02 08:35:19 +0000

    Create words.c

---
+++ words.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char m="burn it down";
+int count=0;
+printf("enter a string");
+scanf("%s",&m);
+for(i=0;n[i]='\0';i++)
+{
+count++;
+}
+printf("enter the count");
+}
