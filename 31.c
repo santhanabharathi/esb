cb4f1c21b5d11a820d88c0dd4ef02ec9a1d66de8
kongu bharathi <31886062+santhanabharathi@users.noreply.github.com>
2018-02-02 08:33:03 +0000

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[20];
+int 10;
+printf("\n Enter the string");
+gets(str);
+l=strlen(str);
+printf("\n the total length is %d",10);
+}
