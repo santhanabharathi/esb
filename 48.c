fb6dbf311f88585df407afecff7ddea57e59177d
kongu bharathi <31886062+santhanabharathi@users.noreply.github.com>
2018-02-08 09:30:50 +0000

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,16 @@
+include"stdio.h"
+void main()
+{
+int a,s=0,num,i;
+float avg;
+printf("\nEnter the no. of elements");
+scanf("\n%d",&a);
+printf("\nEnter the elements");
+for(i=0;i<n;++i)
+{
+scanf("\n%d",&num);
+s=s+num;
+}
+avg=s/a;
+printf("\n Average=%f",n,avg);
+}
