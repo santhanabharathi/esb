3fc76d2f47edb2ac7be2d9cf48794a7af525f243
kongu bharathi <31886062+santhanabharathi@users.noreply.github.com>
2018-02-08 08:44:56 +0000

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char a[200],s[100];
+	printf("enter the string");
+	scanf("%s",&a);
+	printf("enter the 2 nd string");
+	scanf("%s",&s);
+	printf("%s %s",a,s);
+	return 0;
+}
