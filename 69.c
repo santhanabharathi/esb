d04df94ae44360b106fb9261f615ac16bf493b11
kongu bharathi <31886062+santhanabharathi@users.noreply.github.com>
2018-03-01 16:17:08 +0530

    Create 69.c

---
+++ 69.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n3,n2,sub;
+ scanf("%d",&n3);
+    scanf("%d",&n2);
+    sub=n3-n2;
+    
+    if(sub%2==0)
+    {
+    	printf("even");
+	}
+	else
+	{
+		printf("odd");
+	}
+
+    return 0;
+
