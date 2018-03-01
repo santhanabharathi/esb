669e9e217b335a991830761efaa8fd0852b2cc3c
kongu bharathi <31886062+santhanabharathi@users.noreply.github.com>
2018-03-01 16:11:00 +0530

    Create 67.c

---
+++ 67.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,num;
+    scanf("%d",&num);
+    n=num+1;
+    while(n!=0)
+    {
+        if(n%10==0)
+        {
+            printf("%d",n);
+            break;
+        }
+        n++;
+    }
+   
